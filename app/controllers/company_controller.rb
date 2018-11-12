7b52ecd880552fa1b38529ec66603a173b43ba81
Nicolas Debray <nicdebray@gmail.com>
2018-11-12 16:40:37 +0100

    company controller method created

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,5 +1,8 @@
 class CompanyController < ApplicationController
+  before_action :company_find, only: [:show, :edit, :update, :destroy]
+
   def index
+    @companies = Company.all
   end
 
   def show
@@ -9,14 +12,40 @@ class CompanyController < ApplicationController
   end
 
   def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to company_show(@company)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @company = Company.update(company_params)
+      redirect_to company_show_path(@company)
+    else
+      render :new
+    end
   end
 
   def destroy
+    if @company.destroy
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :address, :vat)
+  end
+
+  def company_find
+    @company = Company.find(params[:id])
   end
 end
