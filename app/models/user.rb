813bc1fe5c93854b4eea2bde6fe4a820a8a8a9e4
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-11-12 17:13:46 +0100

    Added validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, :last_name, :user_right, :department, presence: true
 end
