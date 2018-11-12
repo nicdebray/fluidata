522f90af8844ec1636c81c5ce6fd0103e5a22881
Nicolas Debray <nicdebray@gmail.com>
2018-11-12 16:21:51 +0100

    controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'company/index'
+  get 'company/show'
+  get 'company/new'
+  get 'company/create'
+  get 'company/edit'
+  get 'company/update'
+  get 'company/destroy'
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
