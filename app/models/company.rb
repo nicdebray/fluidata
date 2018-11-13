f76a5e7ac33c637852414268524e82c50c2fd3c8
Nicolas Debray <nicdebray@gmail.com>
2018-11-13 18:43:08 +0100

    first lines to fetch data from the Google Analytics API

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,36 @@
+#Inclusion du client Google
+# require 'google/api_client'
+# require 'googleauth'
+require 'google/apis/analytics_v3'
+
 class Company < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :vat, presence: true, uniqueness: true
 
   mount_uploader :photo, PhotoUploader
+
+  def self.call_ga
+    ganalytics = Google::Apis::AnalyticsV3
+    analytics = ganalytics::AnalyticsService.new
+
+    analytics.authorization = ENV['GOOGLE_OUR_SECRET'] #user.token
+
+    #run a query
+    date_range = ganalytics::GaData::Query.new(start_date: '7DaysAgo', end_date: 'today')
+    metric = Google::Apis::AnalyticsV3::GaData::Query.new(metrics: 'ga:sessions')
+    dimension = Google::Apis::AnalyticsV3::GaData::Query.new(dimensions: 'ga:browser')
+
+    request = Google::Apis::AnalyticsreportingV3::GetReportsRequest.new(
+      report_requests: [Google::Apis::AnalyticsreportingV3::ReportRequest.new(
+        view_id: '179537367',
+        metrics: 'ga:sessions',
+        dimensions: 'ga:browser',
+        date_ranges: [{start_date: '7DaysAgo', end_date: 'today'}]
+        )]
+      )
+
+    # response = analytics.batch_get_reports(request)
+    # response.reports
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,10 @@ Bundler.require(*Rails.groups)
 module Fluidata
   class Application < Rails::Application
     config.generators do |generate|
-          generate.assets false
-          generate.helper false
-          generate.test_framework  :test_unit, fixture: false
-        end
+      generate.assets false
+      generate.helper false
+      generate.test_framework  :test_unit, fixture: false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
