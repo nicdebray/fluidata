e6f7ce19f73ff5cb945ae2c208e2f6b609e27c2b
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-11-13 15:00:38 +0100

    models updated

--- app/models/kpi_block.rb
+++ app/models/kpi_block.rb
@@ -1,6 +1,6 @@
 class KpiBlock < ApplicationRecord
   belongs_to :report
-  validates :kpi_type, presence: true, uniqueness: true
+  validates :kpi_type, presence: true
   validates :order, presence: true, uniqueness: true
   validates :start_date, presence: true
   validates :end_date, presence: true
