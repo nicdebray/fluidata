6c959af020f7b173252762ed9c82a788585ee25b
A.W <awittouck@gmail.com>
2018-11-14 15:41:28 +0100

    dropdown fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,19 @@
-//= require rails-ujs
-//= require_tree .
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+import "bootstrap";
+setDropDown()
+
+function setDropDown() {
+
+
+  let button  = document.querySelector("#navbar-wagon-menu")
+
+button.addEventListener( "click", function() {
+    let modal = document.querySelector(".dropdown-menu")
+    console.log(modal.style.display)
+    if( modal.style.display == "block" ) {
+        modal.style.display  = "none"
+    } else {
+        modal.style.display  = "block"
+    }
+})
+
+}
