11d9b04e91261ec0f7692860c150badf26e11da2
Nicolas Debray <nicdebray@gmail.com>
2018-11-14 17:00:56 +0100

    slight google analytics api adaptations

--- app/models/company.rb
+++ app/models/company.rb
@@ -15,33 +15,32 @@ class Company < ApplicationRecord
   def call_ga_v4
     # Set the date range - this is always required for report requests
     date_range = Google::Apis::AnalyticsreportingV4::DateRange.new(
-      start_date: "2018-01-01",
-      end_date: "2018-06-30"
+      start_date: "2018-08-01",
+      end_date: "2018-10-31"
       )
-# Set the metric
-metric = Google::Apis::AnalyticsreportingV4::Metric.new(
-  expression: "ga:users"
-  )
-# Set the dimension
-dimension = Google::Apis::AnalyticsreportingV4::Dimension.new(
-  name: "ga:browser"
-  )
-# Build up our report request and a add country filter
-report_request = Google::Apis::AnalyticsreportingV4::ReportRequest.new(
-  view_id: '179537367',
-  sampling_level: 'DEFAULT',
-  filters_expression: "ga:country==United Kingdom",
-  date_ranges: [date_range],
-  metrics: [metric],
-  dimensions: [dimension]
-  )
-# Create a new report request
-request = Google::Apis::AnalyticsreportingV4::GetReportsRequest.new(
-  { report_requests: [report_request] }
-  )
-# Make API call.
-response = $google_client.batch_get_reports(request)
-
-end
+    # Set the metric
+    metric = Google::Apis::AnalyticsreportingV4::Metric.new(
+      expression: "ga:users"
+      )
+    # Set the dimension
+    dimension = Google::Apis::AnalyticsreportingV4::Dimension.new(
+      name: "ga:browser"
+      )
+    # Build up our report request and a add country filter
+    report_request = Google::Apis::AnalyticsreportingV4::ReportRequest.new(
+      view_id: '179537367',
+      sampling_level: 'DEFAULT',
+      filters_expression: "ga:country==Belgium",
+      date_ranges: [date_range],
+      metrics: [metric],
+      dimensions: [dimension]
+      )
+    # Create a new report request
+    request = Google::Apis::AnalyticsreportingV4::GetReportsRequest.new(
+      { report_requests: [report_request] }
+      )
+    # Make API call.
+    response = $google_client.batch_get_reports(request)
+  end
 
 end
