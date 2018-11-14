aebacb1559f5a5dd7ac8cd1b29a24c3860f3565d
A.W <awittouck@gmail.com>
2018-11-14 17:24:29 +0100

    sign out fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ApplicationRecord
   # validates :first_name, :last_name, :department, presence: true
   has_one :company
   has_many :reports
-
-<<<<<<< HEAD
   validates :department, inclusion: { in: %w(Management, Finance, IT, HR, Production, Marketing, R&D, Sales, Purchase) }
-=======
-  #validates :department, inclusion: %w(Management, Finance, IT, HR, Production, Marketing, R&D, Sales, Purchase)
->>>>>>> 4962c3388a51ee69f6227a47d0f1f9797159ec34
+  # validates :department, inclusion: %w(Management, Finance, IT, HR, Production, Marketing, R&D, Sales, Purchase)
 end
