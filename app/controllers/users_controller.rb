8bfe032e48dc51bda743ff5a5396359535a4cf14
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-11-20 12:00:30 +0100

    created profile pages new and edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :user_right, :department, :admin, :avatar)
+  end
+
+end
+
