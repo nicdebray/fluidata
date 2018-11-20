bf9c83c08bdee5c034e1744591f7462a15b2bc9c
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-11-20 12:25:50 +0100

    made blocks dependent, reports in navbar disappearing if not logged in

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,7 @@
 class Report < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
-  has_many :text_blocks
-  has_many :graph_blocks
-  has_many :kpi_blocks
+  has_many :text_blocks, dependent: :destroy
+  has_many :graph_blocks, dependent: :destroy
+  has_many :kpi_blocks, dependent: :destroy
 end
