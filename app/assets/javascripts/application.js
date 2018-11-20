fbf369185d554156a0039e3da895550e6dcb5887
A.W <awittouck@gmail.com>
2018-11-20 11:00:10 +0100

    uptdating jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,15 @@
 //= require rails-ujs
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
 
 
 
 //= require_tree .
 
+import {} from 'jquery-ujs'
+
+$('#sandbox-container input').datepicker({
+  maxViewMode: 2
+});
