55e2d0a3c610209841ff6fe1d143ae0f7bb193fb
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-11-20 09:56:14 +0100

    fixed avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :user_right, :department, :admin, :avatar])
+  end
+
 end
--- db/migrate/20181119203716_add_column_to_users.rb
+++ db/migrate/20181119203716_add_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :avatar, :string, default: 'avatar_placeholder.png'
+    add_column :users, :avatar, :string
   end
 end
