f15df9665879e04b72ade36630267e37615c3357
Nicolas Debray <nicdebray@gmail.com>
2018-11-23 16:10:54 +0100

    lil changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.prepend( 'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'bootstrap-datepicker': 'bootstrap-datepicker',
+    chartkick: 'chartkick',
+    datetimepicker: 'datetimepicker'
+
+  })
+  )
+
 module.exports = environment
